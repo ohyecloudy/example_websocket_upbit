9114177c4d7e0141a4848e86ee46dee09298e4fa
Oh Jongbin <ohyecloudy@gmail.com>
2021-11-02 23:43:59 +0900

    :tada: mix new

---
+++ lib/example_websocket_upbit.ex
@@ -0,0 +1,18 @@
+defmodule ExampleWebsocketUpbit do
+  @moduledoc """
+  Documentation for `ExampleWebsocketUpbit`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExampleWebsocketUpbit.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/example_websocket_upbit_test.exs
@@ -0,0 +1,8 @@
+defmodule ExampleWebsocketUpbitTest do
+  use ExUnit.Case
+  doctest ExampleWebsocketUpbit
+
+  test "greets the world" do
+    assert ExampleWebsocketUpbit.hello() == :world
+  end
+end
