a5a297770d6adf21f33b4599192314cd9c603408
ohyecloudy <ohyecloudy@gmail.com>
2021-11-26 15:22:24 +0900

    :star: 연결을 유지하기 위해 30초마다 ping 프레임을 보냄
    - init 콜백이 없어서 연결을 할 때, timer를 세팅하고 연결이 끊어지면
      timer를 취소한다

--- lib/example_websocket_upbit/client.ex
+++ lib/example_websocket_upbit/client.ex
@@ -2,8 +2,12 @@ defmodule ExampleWebsocketUpbit.Client do
   use WebSockex
   require Logger
 
+  @ping_interval_ms 30_000
+
+  defstruct [:ping_tick_timer]
+
   def start_link(url) do
-    WebSockex.start_link(url, __MODULE__, :ok, name: __MODULE__)
+    WebSockex.start_link(url, __MODULE__, %__MODULE__{}, name: __MODULE__)
   end
 
   def request_tickers(tickers) do
@@ -21,6 +25,21 @@ defmodule ExampleWebsocketUpbit.Client do
 
   def handle_connect(_conn, state) do
     Logger.notice("Connected!")
+
+    nil = state.ping_tick_timer
+    {:ok, timer} = :timer.send_interval(@ping_interval_ms, self(), :tick)
+    {:ok, put_in(state.ping_tick_timer, timer)}
+  end
+
+  def handle_disconnect(_conn, state) do
+    Logger.notice("Disconnected!")
+
+    {:ok, :cancel} = :timer.cancel(state.ping_tick_timer)
+    {:ok, put_in(state.ping_tick_timer, nil)}
+  end
+
+  def handle_pong(:pong, state) do
+    Logger.debug("pong received")
     {:ok, state}
   end
 
@@ -33,5 +52,15 @@ defmodule ExampleWebsocketUpbit.Client do
   def handle_cast({:send, {type, msg} = frame}, state) do
     Logger.debug("Sending #{type} frame with payload: #{msg}")
     {:reply, frame, state}
+  end
+
+  def handle_cast({:send, type}, state) do
+    Logger.debug("Sending #{type} frame with no payload")
+    {:reply, type, state}
+  end
+
+  def handle_info(:tick, state) do
+    Logger.debug("Sending ping frame")
+    {:reply, :ping, state}
   end
 end
